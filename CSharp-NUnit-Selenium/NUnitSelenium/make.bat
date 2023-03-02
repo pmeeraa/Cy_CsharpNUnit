3be6c4e8075ba96ee1380e45d84393b73963ef40
Meera <pmeeraa@gmail.com>
2023-03-02 17:57:23 +0530

    csharp code added

--- CSharp-NUnit-Selenium/NUnitSelenium/make.bat
+++
@@ -1,4 +0,0 @@
-msbuild /t:clean /p:Configuration=Release
-msbuild /t:clean /p:Configuration=Debug
-msbuild /t:build /p:Configuration=Release
-nunit3-console ParallelSelenium.sln --workers=9 /config:Release
